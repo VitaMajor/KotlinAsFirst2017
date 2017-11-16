07d8666adb2e5ce84985e68385509a53eccfe5d3
Виктор <sfera98@bk.ru>
2017-11-16 23:57:10 +0300

    Cделанa реализация матрицы.

--- src/lesson7/task1/Matrix.kt
+++ src/lesson7/task1/Matrix.kt
@@ -38,32 +38,73 @@ interface Matrix<E> {
  * height = высота, width = ширина, e = чем заполнить элементы.
  * Бросить исключение IllegalArgumentException, если height или width <= 0.
  */
-fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> = TODO()
+fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> {
+    val result = MatrixImpl(height, width, e)
+    for (i in 0 until height) {
+        for (j in 0 until width) {
+            result[i, j] = e
+        }
+    }
+    return result
+}
 
 /**
  * Средняя сложность
  *
  * Реализация интерфейса "матрица"
  */
-class MatrixImpl<E> : Matrix<E> {
-    override val height: Int = TODO()
+class MatrixImpl<E>(override val height: Int, override val width: Int, e: E) : Matrix<E> {
 
-    override val width: Int = TODO()
+    private val list = mutableListOf<MutableList<E>>()
 
-    override fun get(row: Int, column: Int): E  = TODO()
+    init {
+        for (i in 0 until height) {
+            list.add(i,mutableListOf())
+            for (j in 0 until width) {
+                list[i].add(e)
+            }
+        }
+    }
 
-    override fun get(cell: Cell): E  = TODO()
+
+    override fun get(row: Int, column: Int): E = list[row][column]
+
+    override fun get(cell: Cell): E = list[cell.row][cell.column]
 
     override fun set(row: Int, column: Int, value: E) {
-        TODO()
+        list[row][column] = value
     }
 
     override fun set(cell: Cell, value: E) {
-        TODO()
+        list[cell.row][cell.column] = value
     }
 
-    override fun equals(other: Any?) = TODO()
+    override fun equals(other: Any?) =
+            other is MatrixImpl<*> &&
+                    height == other.height &&
+                    width == other.width
 
-    override fun toString(): String = TODO()
+    override fun toString(): String {
+        val sb = StringBuilder()
+        sb.append("[")
+        for (row in 0 until height) {
+            sb.append("[")
+            for (column in 0 until width ) {
+                sb.append(this[row, column])
+            }
+            sb.append("]")
+        }
+        sb.append("]")
+        return "$sb"
+    }
+
+    override fun hashCode(): Int {
+        var result = height
+        result = 31 * result + width
+        result = 31 * result + list.hashCode()
+        return result
+    }
+
+
 }
 
